d918c73b6ceb1e979cb546de18e548895f23f22e
venkatamuraliboyina <b.venkatamurali123@gmail.com>
2024-03-02 14:02:06 -0500

    feat: add city tabs

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -4,11 +4,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class WeatherService {
-url = `https://api.openweathermap.org/data/2.5/forecast?lat=33.44&lon=-94.04&appid=482944e26d320a80bd5e4f23b3de7d1f`;
   constructor() { }
-
-  async getWeatherForCity(): Promise<any> {
-    const data = await fetch(this.url);
+  
+  async getWeatherForCity(city: string): Promise<any> {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=482944e26d320a80bd5e4f23b3de7d1f`;
+    const data = await fetch(url);
     return await data.json();
   }
 
